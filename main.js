09d8877694ba9fb6f4a67e4ec67a54ac99164162
mortiZimmerman <mauricionicolasortiz@gmail.com>
2025-02-18 09:58:29 +0100

    html and css done

--- main.js
+++ main.js
@@ -1,1 +1,5 @@
-const skibidiToilet = "dubdubyesyes";
+const canvas = document.getElementById("tetris");
+const ctx = canvas.getContext("2d");
+const puaseBtn = document.getElementById("pauseButton");
+const scoreElement = document.getElementById("score");
+const highScoreElement = document.getElementById("highScore");
