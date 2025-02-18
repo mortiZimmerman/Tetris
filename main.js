17e1a7d0110f2df0f2c3143fcd8f968e77d32b37
mortiZimmerman <mauricionicolasortiz@gmail.com>
2025-02-18 10:52:43 +0100

    Shapes added

--- main.js
+++ main.js
@@ -3,3 +3,26 @@ const ctx = canvas.getContext("2d");
 const puaseBtn = document.getElementById("pauseButton");
 const scoreElement = document.getElementById("score");
 const highScoreElement = document.getElementById("highScore");
+
+const ROW = 20, COL = 10, SIZE = 30;
+let board = Array.from({ length: ROW }, () => Array(COL).fill(0));
+
+let isPaused = false;
+let gameInterval;
+let score = 0;
+let highScore = localStorage.getItem("highScore") || 0;
+highScoreElement.textContent = highScore;
+
+const SHAPES = [
+    [[1, 1, 1, 1]], //palo horizontal
+    [[1,1],[1,1]], //cuadrado
+    [[0,1,0],[1,1,1]], //T
+    [[1,1,0],[0,1,1]], //Z
+    [[0,1,1],[1,1,0]], //S
+]
+
+let currentPiece ={
+    shape: SHAPES[Math.floor(Math.random() * SHAPES.length)],
+    x:3,
+    y:0
+};
